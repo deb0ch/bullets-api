4a0eb7427ebf399591958d41479d54242a8b6bd0
deb0ch <thomas.de.beauchene@gmail.com>
2018-04-04 19:48:54 +0200

    start using GraphQL

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 
 import hapi                          from 'hapi';
+import { graphqlHapi, graphiqlHapi } from 'apollo-server-hapi';
+import { makeExecutableSchema }      from 'graphql-tools';
 
-import dbPlugin from './dbPlugin';
-import Routes   from './routes';
+import dbPlugin    from './dbPlugin';
+import graphqlTask from './graphql/Task.graphql';
+import resolvers   from './resolvers';
+import routes      from './routes';
+import Task        from './models/Task.js';
 
 
 const server = hapi.Server({
@@ -15,15 +20,42 @@ const server = hapi.Server({
     routes: {cors: true},
 });
 
+const executableSchema = makeExecutableSchema({
+    typeDefs: [graphqlTask],
+    resolvers: resolvers({Task}),
+});
+
 async function main() {
     try {
-        server.route(Routes);
-        await server.register({
-            plugin: dbPlugin,
-            options: {
-                uri: 'mongodb://localhost/bullets-api-test',
-            }
-        });
+        server.route(routes);
+        const plugins = [
+            {
+                plugin: dbPlugin,
+                options: {
+                    uri: 'mongodb://localhost/bullets-api-test',
+                }
+            },
+            {
+                plugin: graphqlHapi,
+                options: {
+                    path: '/graphql',
+                    graphqlOptions: () => ({
+                        pretty: true,
+                        schema: executableSchema,
+                    }),
+                },
+            },
+            {
+                plugin: graphiqlHapi,
+                options: {
+                    path: '/graphiql',
+                    graphiqlOptions: {
+                        endpointURL: '/graphql',
+                    },
+                },
+            },
+        ];
+        await server.register(plugins);
         await server.start();
         console.log(`Server running at: ${server.info.uri}`);
     } catch (err) {
---
+++ resolvers.js
@@ -0,0 +1,35 @@
+
+// import { GraphQLDateTime } from 'graphql-iso-date';
+
+const resolvers = (db) => ({
+		// DateTime: GraphQLDateTime,
+		Query: {
+				async getTasks() {
+						return await db.Task.find();
+				},
+				async getTaskById(root, {id}) {
+						return await db.Task.findById(id);
+				},
+		},
+		Mutation: {
+				async createTask(root, args) {
+						const task = new Task({
+								text: args.text,
+								parent: args.parent || null,
+								checked: false,
+								createdAt: new Date(),
+						});
+						try {
+								const savedTask = await task.save();
+								console.log("saved task: ", task);
+								console.log("savedTask: ", savedTask);
+								return savedTask;
+						} catch (err) {
+								console.log("Error saving task: ", err);
+								return err;
+						}
+				}
+		}
+});
+
+export default resolvers;
